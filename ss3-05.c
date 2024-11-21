a0dfdb972ea24d5e2ba545a26501d6f2a34a0d9b
phuonglinhle268 <plinhle26@gmail.com>
2024-11-21 23:33:43 +0700

    ss3-05

---
+++ ss3-05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	printf("--------------------------------\n");
+	printf("| STT | Ho va ten  | Tuoi | So dien thoai | Email     |\n");
+	printf("--------------------------------\n");
+	
+	print ("| 1 | Nguyen Van A | 20 | 0904488481 | anv@rikkeiacademy.com |\n");
+	print ("| 2 | Nguyen Van B | 21 | 0904488482 | bnv@rikkeiacademy.com |\n");
+	print ("| 3 | Nguyen Van C | 18 | 0904488483 | cnv@rikkeiacademy.com |\n");
+	print ("| 4 | Nguyen Van D | 22 | 0904488484 | dnv@rikkeiacademy.com |\n");
+	print ("| 5 | Nguyen Van E | 19 | 0904488485 | env@rikkeiacademy.com |\n");
+	print ("| 6 | Nguyen Van F | 21 | 0904488486 | fnv@rikkeiacademy.com |\n");
+	print ("| 7 | Nguyen Van G | 23 | 0904488487 | gnv@rikkeiacademy.com |\n");
+	print ("| 8 | Nguyen Van H | 19 | 0904488488 | hnv@rikkeiacademy.com |\n");
+	print ("| 9 | Nguyen Van I | 18 | 0904488489 | inv@rikkeiacademy.com |\n");
+	print ("|10 | Nguyen Van K | 21 | 0904488480 | knv@rikkeiacademy.com |\n");
+	
+	printf("--------------------------------\n");
+	
+	return 0
+}
